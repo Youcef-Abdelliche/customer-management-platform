796ce406ffee160120ba16cbc467b8ddb4111b31
Youcef-Abdelliche <hy_abdelliche@esi.dz>
2020-04-12 22:54:19 +0100

    Add multiple functionalities
    Add update customer function\nAdd delete customer function\nAdd new order function

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,16 @@
-
 from django.urls import path
 from . import views
-
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.products, name='products'),
-    path('customer/', views.customer),
+    path('customer/<int:pk>/', views.customer_info, name='customer'),
+    path('newcustomer/', views.new_customer, name='new_customer'),
+    path('customer/<int:pk>/delete/', views.delete, name='delete'),
+    path('customer/<int:pk>/update/', views.update_view, name='update'),
+    path('customer/<int:pk>/update_customer/', views.update_customer, name='update_customer'),
+    path('new_customer/', views.new_customer_view, name='new_customer_view'),
+    path('newcustomer/', views.new_customer, name='new_customer'),
+    path('new_order_view/', views.new_order_view, name='new_order_view'),
+    path('new_order/', views.new_order, name='new_order'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,20 +1,98 @@
 from django.shortcuts import render
 from .models import *
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseRedirect
 
 
 def home(request):
-    customers = Customer.objects.all().order_by("-date_created")
+    customers = Customer.objects.all().order_by("date_created")
     orders = Order.objects.all().order_by("-date_created")
+    last5_order = orders[0:5]
     orders_delivered = Order.objects.filter(status="Delivered")
     orders_pending = Order.objects.filter(status="Pending")
     context = {'customers': customers, 'orders': orders, 'orders_delivered': orders_delivered,
-               'orders_pending': orders_pending}
+               'orders_pending': orders_pending, 'last_orders': last5_order}
     return render(request, 'accounts/dashboard.html', context)
 
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    Products = Product.objects.all().order_by("name")
+
+    context = {'products': Products}
+    return render(request, 'accounts/products.html', context)
 
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer_info(request, pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all
+
+    context = {'customer': customer, 'orders': orders}
+    return render(request, 'accounts/customer.html', context)
+
+
+def new_customer(request):
+    return render(request, 'accounts/new_customer.html')
+
+
+def delete_view(request, pk):
+    return render(request, )
+
+
+def delete(request, pk):
+    Customer.objects.get(id=pk).delete()
+    return HttpResponseRedirect('/')
+
+
+def update_view(request, pk):
+    customer = Customer.objects.get(id=pk)
+    context = {'customer': customer}
+    return render(request, 'accounts/customerInfo.html', context)
+
+
+def update_customer(request, pk):
+    customer = Customer.objects.get(id=pk)
+
+    customer_name = request.POST.get('customerName')
+    customer_email = request.POST.get('customerEmail')
+    customer_phone = request.POST.get('customerPhone')
+
+    customer.name = customer_name
+    customer.email = customer_email
+    customer.phone = customer_phone
+    customer.save()
+
+    return HttpResponseRedirect('/')
+
+
+def new_customer_view(request):
+    return render(request, 'accounts/new_customer.html')
+
+
+def new_customer(request):
+
+    customer_name = request.POST.get('customerName')
+    customer_email = request.POST.get('customerEmail')
+    customer_phone = request.POST.get('customerPhone')
+
+    Customer.objects.create(name=customer_name, email=customer_email, phone=customer_phone)
+
+    return HttpResponseRedirect('/')
+
+
+def new_order_view(request):
+    customers = Customer.objects.all()
+    orders_status = Order.STATUS
+    products_list = Product.objects.all()
+
+    context = {'customers': customers, 'orders_status': orders_status, 'products': products_list}
+    return render(request, 'accounts/new_order.html', context)
+
+
+def new_order(request):
+
+    order_customer = Customer.objects.get(name=request.GET.get('order_customer'))
+    order_product = Product.objects.get(name=request.GET.get('order_product'))
+    order_status = request.GET.get('order_status')
+
+    Order.objects.create(customer=order_customer, product=order_product, status=order_status)
+    return HttpResponseRedirect('/')
